6890276d9e5dc53566a70be38de77900136b4507
River <riverfor@gmail.com>
2019-05-27 18:04:43 +0800

    Clean code

--- src/main/java/com/google/zxing/client/android/result/TextResultHandler.java
+++ src/main/java/com/google/zxing/client/android/result/TextResultHandler.java
@@ -19,8 +19,22 @@ package com.google.zxing.client.android.result;
 import com.google.zxing.Result;
 import com.google.zxing.client.android.R;
 import com.google.zxing.client.result.ParsedResult;
+import com.quseit.base.MyApp;
+import com.quseit.config.CONF;
+import com.quseit.util.FileHelper;
+import com.quseit.util.Log;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.MessageFormat;
 
 /**
  * This class handles TextParsedResult as well as unknown formats. It's the fallback handler.
@@ -29,15 +43,26 @@ import android.app.Activity;
  */
 public final class TextResultHandler extends ResultHandler {
 
+  private final String TAG = "TextResultHandler";
+  Activity activity;
   private static final int[] buttons = {
-      R.string.button_web_search,
+          R.string.button_edit,
+          R.string.button_run_with_py,
+          R.string.button_run_as_parameter,
+          R.string.button_custom_product_search,
+
+
+/*      R.string.button_web_search,
       R.string.button_share_by_email,
       R.string.button_share_by_sms,
       R.string.button_custom_product_search,
+      */
   };
 
   public TextResultHandler(Activity activity, ParsedResult result, Result rawResult) {
     super(activity, result, rawResult);
+    this.activity = activity;
+
   }
 
   @Override
@@ -55,16 +80,19 @@ public final class TextResultHandler extends ResultHandler {
     String text = getResult().getDisplayResult();
     switch (index) {
       case 0:
-        webSearch(text);
+        viewScript(text);
+        //webSearch(text);
         break;
       case 1:
-        shareByEmail(text);
+        runScript(text);
+        //shareByEmail(text);
         break;
       case 2:
-        shareBySMS(text);
+        runAsParameter(text);
+        //shareBySMS(text);
         break;
       case 3:
-        openURL(fillInCustomSearchURL(text));
+        //openURL(fillInCustomSearchURL(text));
         break;
     }
   }
@@ -72,5 +100,86 @@ public final class TextResultHandler extends ResultHandler {
   @Override
   public int getDisplayTitle() {
     return R.string.result_text;
+  }
+
+
+
+  public File getCodeAsFile(String text, String tmp) {
+    try {
+      File pyCache = FileHelper.getBasePath(Environment.getExternalStorageDirectory().getAbsolutePath(),"qpython/cache");
+      File py = new File(pyCache+"/"+tmp);
+
+      byte[] data = text.getBytes();
+      FileOutputStream outStream = null;
+
+      outStream = new FileOutputStream(py);
+      outStream.write(data);
+      outStream.close();
+
+      return py;
+
+    } catch (FileNotFoundException e) {
+      Toast.makeText(this.activity.getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+      e.printStackTrace();
+    } catch (IOException e) {
+      Toast.makeText(this.activity.getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+      e.printStackTrace();
+
+    }
+    return null;
+
+  }
+
+  public void runAsParameter(String text) {
+
+    Log.d(TAG, "runAsParameter");
+
+    File py = getCodeAsFile(text,".last_param");
+
+    Intent it = new Intent();
+    it.setClassName(this.activity.getPackageName(), "org.qpython.qpylib.MPyApi");
+    it.setAction(Intent.ACTION_PICK);
+
+    Uri uri = Uri.fromFile(py);
+
+    it.setDataAndType(uri, "text/x-python");
+    this.activity.startActivity(it);
+
+
+  }
+
+  public void runScript(String text) {
+    Log.d(TAG, "runScript");
+
+    File py = getCodeAsFile(text, ".last_tmp.py");
+
+    Intent it = new Intent();
+    it.setClassName(this.activity.getPackageName(), "org.qpython.qpylib.MPyApi");
+    it.setAction(Intent.ACTION_RUN);
+
+    Uri uri = Uri.fromFile(py);
+
+    it.setDataAndType(uri, "text/x-python");
+    this.activity.startActivity(it);
+
+
+  }
+
+  public void viewScript(String text) {
+    Log.d(TAG, "viewScript");
+
+    File py = getCodeAsFile(text, ".last_tmp.py");
+
+    Intent it = new Intent();
+    it.setClassName(this.activity.getPackageName(), "com.quseit.texteditor.TedActivity");
+    it.setAction(Intent.ACTION_VIEW);
+
+    Uri uri = Uri.fromFile(py);
+
+    it.setDataAndType(uri, "text/x-python");
+    this.activity.startActivity(it);
+
+    //Toast.makeText(getApplicationContext(), MessageFormat.format(getString(R.string.saved_as), pyCache.getName()), Toast.LENGTH_SHORT).show();
   }
 }
